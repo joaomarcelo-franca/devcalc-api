41e7cb2d6bdf2aff7c850de088a9b2c4c62a62e1
joaomarcelo-franca <joao.franca@al.infnet.edu.br>
2025-12-09 19:08:41 -0300

    commit inicial - prints e ReadME

---
+++ README.md
@@ -0,0 +1,7 @@
+# DevCalc API
+
+Projeto DevCalc — API Java para testes de CI/CD.
+
+Build: Maven
+
+(README final será escrito depois)
---
+++ src/main/java/com/devcalc/App.java
@@ -0,0 +1,8 @@
+package com.devcalc;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
