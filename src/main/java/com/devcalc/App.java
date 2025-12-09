375ef01129159427ace4c377ad5190b76710ffff
joaomarcelo-franca <joao.franca@al.infnet.edu.br>
2025-12-09 19:18:03 -0300

    implementando calculator endpoints e test

--- src/main/java/com/devcalc/App.java
+++ src/main/java/com/devcalc/App.java
@@ -4,5 +4,37 @@ import io.javalin.Javalin;
 
 public class App {
     public static void main(String[] args) {
+
+        CalculatorService service = new CalculatorService();
+
+        Javalin app = Javalin.create().start(7000);
+
+        app.get("/add", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.json(service.add(a, b));
+        });
+
+        app.get("/subtract", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.json(service.subtract(a, b));
+        });
+
+        app.get("/multiply", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.json(service.multiply(a, b));
+        });
+
+        app.get("/divide", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            try {
+                ctx.json(service.divide(a, b));
+            } catch (ArithmeticException exception){
+                ctx.status(400).result(exception.getMessage());
+            }
+        });
     }
 }
---
+++ src/main/java/com/devcalc/CalculatorService.java
@@ -0,0 +1,22 @@
+package com.devcalc;
+
+public class CalculatorService {
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+    public double divide(double a, double b){
+        if (b == 0){
+            throw new ArithmeticException("Division by zero does not exist.");
+        }
+        return a / b;
+    }
+}
